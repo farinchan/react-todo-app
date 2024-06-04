bf34809c61d8f64f5eb0897a5a08aae31ec55593
fajri <fajri@gariskode.com>
2024-06-04 20:08:30 +0700

    Develop toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const Todos = ({todos}) => {
+const Todos = ({todos, toggleCompleted}) => {
 
     return (
         <div style={styles.container}>
             {todos.map((todo)=>{
-                return <TodoItem key={todo.id} todo={todo} />
+                return <TodoItem key={todo.id} todo={todo} toggleCompleted={toggleCompleted} />
             })}
         </div>
     );
